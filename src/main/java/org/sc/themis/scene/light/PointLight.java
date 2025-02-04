8844be30205cbf70a720deb44edeae507540a35f
Stéphane <Korbier@users.noreply.github.com>
2025-02-04 19:24:21 +0100

    TMS-4 feat Add Point light #close

---
+++ src/main/java/org/sc/themis/scene/light/PointLight.java
@@ -0,0 +1,38 @@
+package org.sc.themis.scene.light;
+
+import org.joml.Vector3f;
+import org.sc.themis.shared.utils.MemorySizeUtils;
+
+public class PointLight extends Light {
+
+    public final static int SIZE = Light.SIZE + MemorySizeUtils.VEC3F + MemorySizeUtils.VEC4F;
+
+    private final Vector3f position;
+    private final Attenuation attenuation;
+
+    public PointLight(Vector3f ambient, Vector3f diffuse, Vector3f specular, Vector3f position, Attenuation attenuation) {
+        super(ambient, diffuse, specular);
+        this.attenuation = Attenuation.of(attenuation);
+        this.position = new Vector3f( position );
+        setDirty();
+    }
+
+    public Vector3f getPosition() {
+        return new Vector3f(position);
+    }
+
+    public void setPosition(Vector3f position) {
+        this.position.set( position );
+        setDirty();
+    }
+
+    public Attenuation getAttenuation() {
+        return Attenuation.of(attenuation);
+    }
+
+    public void setAttenuation(Attenuation attenuation) {
+        this.attenuation.set( attenuation );
+        setDirty();
+    }
+
+}
