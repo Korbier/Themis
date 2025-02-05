420d09eb59e7b0195fe14c6f1a83de740d2e6395
Stéphane <stephane.charenton@gmail.com>
2025-02-05 18:07:29 +0100

    TMS-16 Checkstyle #close

--- src/main/java/org/sc/themis/shared/assertion/Assertions.java
+++ src/main/java/org/sc/themis/shared/assertion/Assertions.java
@@ -1,46 +1,91 @@
 package org.sc.themis.shared.assertion;
 
-import org.sc.themis.shared.exception.ThemisException;
-
 import java.util.Collection;
 import java.util.Objects;
 import java.util.function.BooleanSupplier;
 import java.util.function.Predicate;
+import org.sc.themis.shared.exception.ThemisException;
 
+/**
+ * Assertions used to check validity of method input and trigger contextual exception.
+ */
 public class Assertions {
 
-    private Assertions() {}
+    /**
+     * Private constructor.
+     */
+    private Assertions() {
+    }
 
+    /**
+     * Not null assertion.
+     *
+     * @param objToCheck Object to check
+     * @param rejected Exception rejected if assertion fails
+     * @param <E> Exception type
+     * @throws E Rejected exception
+     */
     public static <E extends ThemisException> void notNull(Object objToCheck, E rejected) throws E {
-        if ( Objects.isNull( objToCheck ) ) {
+        if (Objects.isNull(objToCheck)) {
             throw rejected;
         }
     }
 
-    public static <E extends ThemisException> void notEmpty( Collection<?> objToCheck, E rejected) throws E {
-        if ( objToCheck.isEmpty() ) {
+    /**
+     * Not empty assertion.
+     *
+     * @param objToCheck Object to check
+     * @param rejected Exception rejected if assertion fails
+     * @param <E> Exception type
+     * @throws E Rejected exception
+     */
+    public static <E extends ThemisException> void notEmpty(Collection<?> objToCheck, E rejected) throws E {
+        if (objToCheck.isEmpty()) {
             throw rejected;
         }
     }
 
-
+    /**
+     * is valid assertion.
+     *
+     * @param objToCheck Object to check
+     * @param predicate predicate used to check validity
+     * @param rejected Exception rejected if assertion fails
+     * @param <E> Exception type
+     * @throws E Rejected exception
+     */
     public static <O, E extends ThemisException> void isValid(O objToCheck, Predicate<O> predicate, E rejected) throws E {
-        if ( !predicate.test( objToCheck ) ) {
+        if (!predicate.test(objToCheck)) {
             throw rejected;
         }
     }
 
+    /**
+     * is true assertion.
+     *
+     * @param predicate predicate used to check validity
+     * @param rejected Exception rejected if assertion fails
+     * @param <E> Exception type
+     * @throws E Rejected exception
+     */
     public static <E extends ThemisException> void isTrue(BooleanSupplier predicate, E rejected) throws E {
-        if ( !predicate.getAsBoolean() ) {
+        if (!predicate.getAsBoolean()) {
             throw rejected;
         }
     }
 
+    /**
+     * is false assertion.
+     *
+     * @param predicate predicate used to check validity
+     * @param rejected Exception rejected if assertion fails
+     * @param <E> Exception type
+     * @throws E Rejected exception
+     */
     public static <E extends ThemisException> void isFalse(BooleanSupplier predicate, E rejected) throws E {
-        if ( predicate.getAsBoolean() ) {
+        if (predicate.getAsBoolean()) {
             throw rejected;
         }
     }
-
 
 }
--- src/main/java/org/sc/themis/shared/utils/ArrayUtils.java
+++ src/main/java/org/sc/themis/shared/utils/ArrayUtils.java
@@ -2,14 +2,26 @@ package org.sc.themis.shared.utils;
 
 import java.util.Arrays;
 
+/**
+ * Array centered tools.
+ */
 public class ArrayUtils {
 
-    private ArrayUtils() {}
+    private ArrayUtils() {
+    }
 
-    public static <O> O [] merge( O object, O ... others) {
-        O [] copy = Arrays.copyOf( others, others.length + 1 );
+    /**
+     * Merge an object and arrays in one array.
+     *
+     * @param object Object to merge
+     * @param others Arrays to merge
+     * @param <O>    Object type
+     * @return An array contains all input data
+     */
+    public static <O> O[] merge(O object, O... others) {
+        O[] copy = Arrays.copyOf(others, others.length + 1);
         copy[0] = object;
-        System.arraycopy( others, 0, copy, 1, others.length );
+        System.arraycopy(others, 0, copy, 1, others.length);
         return copy;
     }
 
