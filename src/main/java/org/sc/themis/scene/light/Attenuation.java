bcd80146442eb4c5c8ad9a8171e43106132292e4
Stéphane <Korbier@users.noreply.github.com>
2025-02-05 18:08:02 +0100

    TMS-16 Checkstyle #close

--- src/main/java/org/sc/themis/scene/light/Attenuation.java
+++ src/main/java/org/sc/themis/scene/light/Attenuation.java
@@ -2,64 +2,105 @@ package org.sc.themis.scene.light;
 
 import org.joml.Vector4f;
 
+/**
+ * Light attenuation.
+ */
 public class Attenuation {
 
     private EnumAttenuation type;
     private final Vector4f data = new Vector4f();
 
-    public static Attenuation of( Attenuation other ) {
+    /**
+     * Create an attenuation from another (copy).
+     *
+     * @param other Other attenuation to copy from
+     * @return A new attenuation
+     */
+    public static Attenuation of(Attenuation other) {
         Attenuation attenuation = new Attenuation();
         attenuation.type = other.type;
-        attenuation.data.set( other.data );
+        attenuation.data.set(other.data);
         return attenuation;
     }
 
-    //https://lisyarus.github.io/blog/posts/point-light-attenuation.html
-    public static Attenuation type1( float radius, float falloff ) {
+    /**
+     *  Create an attenuation of type 1.
+     *  Documentation : <a href=" https://lisyarus.github.io/blog/posts/point-light-attenuation.html">here</a>
+     *
+     * @param radius Attenuation radius
+     * @param falloff Attenuation falloff
+     * @return A new attenuation
+     */
+    public static Attenuation type1(float radius, float falloff) {
         Attenuation attenuation = new Attenuation();
         attenuation.type = EnumAttenuation.TYPE_1;
-        attenuation.data.set( 1.0f, radius, falloff, 0.0f);
+        attenuation.data.set(1.0f, radius, falloff, 0.0f);
         return  attenuation;
     }
 
-    //https://lisyarus.github.io/blog/posts/point-light-attenuation.html
-    public static Attenuation type2( float radius, float falloff ) {
+    /**
+     *  Create an attenuation of type 2.
+     *  Documentation : <a href=" https://lisyarus.github.io/blog/posts/point-light-attenuation.html">here</a>
+     *
+     * @param radius Attenuation radius
+     * @param falloff Attenuation falloff
+     * @return A new attenuation
+     */
+    public static Attenuation type2(float radius, float falloff) {
         Attenuation attenuation = new Attenuation();
         attenuation.type = EnumAttenuation.TYPE_2;
-        attenuation.data.set( 2.0f, radius, falloff, 0.0f);
+        attenuation.data.set(2.0f, radius, falloff, 0.0f);
         return  attenuation;
     }
 
     private Attenuation() {}
 
-    public void set( Attenuation other ) {
-        this.data.set( other.data );
+    /**
+     *  Set data from another attenuation object.
+     *
+     * @param other Source attenuation
+     */
+    public void set(Attenuation other) {
+        this.data.set(other.data);
     }
 
     public float getType() {
         return this.data.x;
     }
 
-    public void setRadius( float newRadius ) {
-        switch ( this.type ) {
+    /**
+     *  Set radius.
+     *
+     * @param newRadius new radius value
+     */
+    public void setRadius(float newRadius) {
+        switch (this.type) {
             case  TYPE_1, TYPE_2 -> this.data.y = newRadius;
+            default -> {}
         }
     }
 
     public float getRadius() {
-        return switch ( this.type ) {
+        return switch (this.type) {
             case  TYPE_1, TYPE_2 -> this.data.y;
+            default -> -0.0f;
         };
     }
 
-    public void setFallOff( float newFallOff ) {
-        switch ( this.type ) {
+    /**
+     *  Set fallof.
+     *
+     * @param newFallOff new fallof value
+     */
+    public void setFallOff(float newFallOff) {
+        switch (this.type) {
             case  TYPE_1, TYPE_2 -> this.data.z = newFallOff;
+            default -> {}
         }
     }
 
     public float getFallOff() {
-        return switch ( this.type ) {
+        return switch (this.type) {
             case  TYPE_1, TYPE_2 -> this.data.z;
         };
     }
@@ -76,6 +117,12 @@ public class Attenuation {
         return this.data.w;
     }
 
+    /**
+     *  Return raw data.
+     *
+     * @return raw data
+     *
+     */
     public Vector4f data() {
         return this.data;
     }
